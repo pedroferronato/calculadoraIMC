8648b465cc10492ba7c09d2cc2c3dfcdcd6049e8
pedroferronato <54194291+pedroferronato@users.noreply.github.com>
2020-10-14 21:02:44 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ function limparFormulario(){
 
 document.querySelector('#btnCalcularIMC').addEventListener('click', (event) => {
     event.preventDefault()
+    document.getElementById("magreza").classList.remove("combinaResultado")
+    document.getElementById("normal").classList.remove("combinaResultado")
+    document.getElementById("sobrepeso").classList.remove("combinaResultado")
+    document.getElementById("obesidade").classList.remove("combinaResultado")
+    document.getElementById("obesidadeGrave").classList.remove("combinaResultado")
     let imc = parseFloat(calcularIMC(peso.value, altura.value).toFixed(2))
     resultado.innerHTML = '<h3 class="txtResultado">Seu IMC é: ' + imc.toString() + '<h3>'
     let tabela = document.querySelector("table")
@@ -24,11 +29,11 @@ document.querySelector('#btnCalcularIMC').addEventListener('click', (event) => {
         row = "magreza"
     } else if (imc >= 18.5 && imc <= 24.9) {
         row = "normal"
-    } else if (imc >= 25,0 && imc <= 29.9) {
+    } else if (imc >= 25.0 && imc <= 29.9) {
         row = "sobrepeso"
     } else if (imc >= 30.0 && imc <= 39.9) {
         row = "obesidade"
-    } else {
+    } else if (imc >= 40.0){
         row = "obesidadeGrave"
     }
     document.getElementById(row).classList.add("combinaResultado")
